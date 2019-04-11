46627f669258edc3651b1c61e70118134c79a488
achellko <biara.prime@gmail.com>
2019-04-11 10:17:55 +0200

    func

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 import React from "react";
+import "./styles.css";
 import PropTypes from "prop-types";
 
-const HelloWorld = ({ where, increment }) => {
+function warunkowa(increment) {
+  if (increment === "Świecie") return " swiat";
+  else return " pieklo";
+}
+
+function moje_hello(props) {
+  let wartosc = props.increment ? <span>z krokiem {props.increment}</span> : "";
   return (
     <p>
-      Hello {where} z krokiem {increment}
+      Hello {props.where}
+      {warunkowa(props.where)}
     </p>
   );
-};
+}
+
+const HelloWorld = moje_hello;
+//   return (
+//     <p>
+//       Hello {where} z krokiem {increment}
+//     </p>
+//   );
+// };
 HelloWorld.propTypes = {
   where: PropTypes.string.isRequired,
   increment: PropTypes.func.isRequired
